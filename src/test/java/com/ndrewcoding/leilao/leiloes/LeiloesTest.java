4152ec36d03d481a1607464e452960e7d9f271ec
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-28 20:24:12 -0300

    Test: setup basic structure of LeiloesTest

--- src/test/java/com/ndrewcoding/leilao/leiloes/LeiloesTest.java
+++ src/test/java/com/ndrewcoding/leilao/leiloes/LeiloesTest.java
@@ -1,4 +1,20 @@
 package com.ndrewcoding.leilao.leiloes;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
 public class LeiloesTest {
+
+    private LeiloesPage leiloesPage;
+
+    @BeforeEach
+    void setUp() {
+        this.leiloesPage = new LeiloesPage();
+    }
+
+    @AfterEach
+    void teardown() {
+        leiloesPage.fechar();
+    }
+
 }
