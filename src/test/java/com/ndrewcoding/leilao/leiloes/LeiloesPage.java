69955358c6197186665d5bfef8e97a5bb135edc8
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-28 20:23:55 -0300

    Feat: setup basic structure of LeiloesPage

--- src/test/java/com/ndrewcoding/leilao/leiloes/LeiloesPage.java
+++ src/test/java/com/ndrewcoding/leilao/leiloes/LeiloesPage.java
@@ -1,4 +1,21 @@
 package com.ndrewcoding.leilao.leiloes;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class LeiloesPage {
+
+    private final WebDriver driver;
+    private static final String URL_LEILOES = "http://localhost:8080/login";
+
+    public LeiloesPage() {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+        driver = new ChromeDriver();
+        driver.navigate().to(URL_LEILOES);
+    }
+
+    public void fechar() {
+        this.driver.quit();
+    }
+
 }
