836eea26950f67ffe21a21beb3f608e9cd732b0f
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-28 19:32:53 -0300

    Test: implement test 'naoDeveriaEfetuarLoginComDadosInvalidos()' in LoginTest

--- src/test/java/com/ndrewcoding/leilao/login/LoginTest.java
+++ src/test/java/com/ndrewcoding/leilao/login/LoginTest.java
@@ -3,6 +3,7 @@ package com.ndrewcoding.leilao.login;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -24,6 +25,27 @@ public class LoginTest {
 
         Assertions.assertNotEquals("http://localhost:8080/login", driver.getCurrentUrl());
         Assertions.assertEquals("fulano", driver.findElement(By.id("usuario-logado")).getText());
+
+        driver.quit();
+    }
+
+    @Test
+    public void naoDeveriaEfetuarLoginComDadosInvalidos() {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.navigate().to("http://localhost:8080/login");
+
+        driver.findElement(By.id("username")).sendKeys("invalido");
+
+        driver.findElement(By.id("password")).sendKeys("123");
+
+        driver.findElement(By.id("login-form")).submit();
+
+        Assertions.assertEquals("http://localhost:8080/login?error", driver.getCurrentUrl());
+        Assertions.assertThrows(NoSuchElementException.class, () -> driver.findElement(By.id("usuario-logado")));
+        Assertions.assertTrue(driver.getPageSource().contains("Usuário e senha inválidos."));
 
         driver.quit();
     }
