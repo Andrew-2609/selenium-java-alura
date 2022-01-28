640338a730265589a34da260f4724798b2c366ed
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-28 09:22:23 -0300

    Test: implement first test of the application

--- src/test/java/com/ndrewcoding/leilao/HelloWorldSelenium.java
+++ src/test/java/com/ndrewcoding/leilao/HelloWorldSelenium.java
@@ -1,4 +1,17 @@
 package com.ndrewcoding.leilao;
 
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class HelloWorldSelenium {
+
+    @Test
+    public void helloWorld() {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.navigate().to("http://localhost:8080/leilao");
+        webDriver.quit();
+    }
+
 }
