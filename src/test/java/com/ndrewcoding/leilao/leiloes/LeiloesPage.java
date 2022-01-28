9e22c90422c2ae215edc9db945daff52248102b4
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-28 20:32:47 -0300

    Feat: create method carregarFormularioDeCadastroDeLeilao() in LeiloesPage

--- src/test/java/com/ndrewcoding/leilao/leiloes/LeiloesPage.java
+++ src/test/java/com/ndrewcoding/leilao/leiloes/LeiloesPage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 public class LeiloesPage {
 
     private final WebDriver driver;
+    private static final String URL_CADASTRO_LEILAO = "http://localhost:8080/leiloes/new";
 
     public LeiloesPage(WebDriver webDriver) {
         this.driver = webDriver;
@@ -12,6 +13,11 @@ public class LeiloesPage {
 
     public void fechar() {
         this.driver.quit();
+    }
+
+    public CadastroLeilaoPage carregarFormularioDeCadastroDeLeilao() {
+        this.driver.navigate().to(URL_CADASTRO_LEILAO);
+        return new CadastroLeilaoPage(driver);
     }
 
 }
