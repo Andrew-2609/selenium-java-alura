858a6e87d41b781c890ad8cd5e32841ad2f05a12
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-28 20:31:58 -0300

    Feat: implement basic structure of CadastroLeilaoPage

--- src/test/java/com/ndrewcoding/leilao/leiloes/CadastroLeilaoPage.java
+++ src/test/java/com/ndrewcoding/leilao/leiloes/CadastroLeilaoPage.java
@@ -3,6 +3,11 @@ package com.ndrewcoding.leilao.leiloes;
 import org.openqa.selenium.WebDriver;
 
 public class CadastroLeilaoPage {
-    public CadastroLeilaoPage(WebDriver driver) {
+
+    private final WebDriver driver;
+
+    public CadastroLeilaoPage(WebDriver webDriver) {
+        this.driver = webDriver;
     }
+
 }
