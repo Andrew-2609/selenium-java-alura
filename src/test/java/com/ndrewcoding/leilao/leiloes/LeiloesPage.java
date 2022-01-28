931c7ac42f04c66c24d332041c1812bbcd067944
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-28 20:28:54 -0300

    Refactor: clean unnecessary code of LeiloesPage based on new logic with LoginPage

--- src/test/java/com/ndrewcoding/leilao/leiloes/LeiloesPage.java
+++ src/test/java/com/ndrewcoding/leilao/leiloes/LeiloesPage.java
@@ -1,17 +1,13 @@
 package com.ndrewcoding.leilao.leiloes;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LeiloesPage {
 
     private final WebDriver driver;
-    private static final String URL_LEILOES = "http://localhost:8080/login";
 
-    public LeiloesPage() {
-        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
-        driver = new ChromeDriver();
-        driver.navigate().to(URL_LEILOES);
+    public LeiloesPage(WebDriver webDriver) {
+        this.driver = webDriver;
     }
 
     public void fechar() {
