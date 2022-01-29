abbe13f9fa05e83ac5205a91e7aec4a7e535a758
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-28 21:52:16 -0300

    Refactor: make LeiloesPage a subclass of PageObject

--- src/test/java/com/ndrewcoding/leilao/leiloes/LeiloesPage.java
+++ src/test/java/com/ndrewcoding/leilao/leiloes/LeiloesPage.java
@@ -1,29 +1,25 @@
 package com.ndrewcoding.leilao.leiloes;
 
+import com.ndrewcoding.leilao.PageObject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class LeiloesPage {
+public class LeiloesPage extends PageObject {
 
-    private final WebDriver driver;
     private static final String URL_LEILOES = "http://localhost:8080/leiloes";
 
     public LeiloesPage(WebDriver webDriver) {
-        this.driver = webDriver;
-    }
-
-    public void fechar() {
-        this.driver.quit();
+        super(webDriver);
     }
 
     public CadastroLeilaoPage carregarFormularioDeCadastroDeLeilao() {
-        this.driver.navigate().to(URL_LEILOES.concat("/new"));
-        return new CadastroLeilaoPage(driver);
+        this.webDriver.navigate().to(URL_LEILOES.concat("/new"));
+        return new CadastroLeilaoPage(webDriver);
     }
 
     public boolean verificarSeLeilaoEstaCadastrado(String nome, String dataAbertura, String valorInicial) {
-        WebElement ultimaLinhaDaTabelaDeLeiloes = this.driver.findElement(By.cssSelector("#tabela-leiloes tbody tr:last-child"));
+        WebElement ultimaLinhaDaTabelaDeLeiloes = this.webDriver.findElement(By.cssSelector("#tabela-leiloes tbody tr:last-child"));
         WebElement colunaNome = ultimaLinhaDaTabelaDeLeiloes.findElement(By.cssSelector("td:nth-child(1)"));
         WebElement colunaDataAbertura = ultimaLinhaDaTabelaDeLeiloes.findElement(By.cssSelector("td:nth-child(2)"));
         WebElement colunaValorInicial = ultimaLinhaDaTabelaDeLeiloes.findElement(By.cssSelector("td:nth-child(3)"));
@@ -32,7 +28,7 @@ public class LeiloesPage {
     }
 
     public boolean estaNaPaginaAtual() {
-        return this.driver.getCurrentUrl().equals(URL_LEILOES);
+        return this.webDriver.getCurrentUrl().equals(URL_LEILOES);
     }
 
 }
