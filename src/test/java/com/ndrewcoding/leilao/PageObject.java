12d0dd271c2fbe67049cd3c9ed7d6bcf13128c1f
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-28 21:58:16 -0300

    Feat: manage basic timeouts of WebDriver in PageObject

--- src/test/java/com/ndrewcoding/leilao/PageObject.java
+++ src/test/java/com/ndrewcoding/leilao/PageObject.java
@@ -3,6 +3,8 @@ package com.ndrewcoding.leilao;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public abstract class PageObject {
 
     protected WebDriver webDriver;
@@ -11,6 +13,11 @@ public abstract class PageObject {
         System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
 
         this.webDriver = webDriver == null ? new ChromeDriver() : webDriver;
+
+        this.webDriver.manage().timeouts()
+                .implicitlyWait(5, TimeUnit.SECONDS)
+                .pageLoadTimeout(10, TimeUnit.SECONDS)
+        ;
     }
 
     public void fechar() {
