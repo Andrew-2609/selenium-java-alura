8ea963a2e60d5e08535999d0884d5320c8c25c8a
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-28 21:52:28 -0300

    Refactor: make CadastroLeilaoPage a subclass of PageObject

--- src/test/java/com/ndrewcoding/leilao/leiloes/CadastroLeilaoPage.java
+++ src/test/java/com/ndrewcoding/leilao/leiloes/CadastroLeilaoPage.java
@@ -1,29 +1,28 @@
 package com.ndrewcoding.leilao.leiloes;
 
+import com.ndrewcoding.leilao.PageObject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class CadastroLeilaoPage {
-
-    private final WebDriver driver;
+public class CadastroLeilaoPage extends PageObject {
 
     public CadastroLeilaoPage(WebDriver webDriver) {
-        this.driver = webDriver;
+        super(webDriver);
     }
 
     public void preencherFormularioDeCadastro(String nome, String dataAbertura, String valorInicial) {
-        this.driver.findElement(By.id("nome")).sendKeys(nome);
-        this.driver.findElement(By.id("dataAbertura")).sendKeys(dataAbertura);
-        this.driver.findElement(By.id("valorInicial")).sendKeys(valorInicial);
+        this.webDriver.findElement(By.id("nome")).sendKeys(nome);
+        this.webDriver.findElement(By.id("dataAbertura")).sendKeys(dataAbertura);
+        this.webDriver.findElement(By.id("valorInicial")).sendKeys(valorInicial);
     }
 
     public LeiloesPage submeterFormularioDeCadastro() {
-        this.driver.findElement(By.id("button-submit")).submit();
-        return new LeiloesPage(this.driver);
+        this.webDriver.findElement(By.id("button-submit")).submit();
+        return new LeiloesPage(this.webDriver);
     }
 
     public boolean estaExibindoMensagensDeValidacao() {
-        String pageSource = this.driver.getPageSource();
+        String pageSource = this.webDriver.getPageSource();
 
         boolean minimoDeCaracteresAtingido = pageSource.contains("minimo");
         boolean campoEstaEmBranco = pageSource.contains("não deve estar em branco");
