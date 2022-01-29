26944f244b18365bcb3ed44350d2997eb812baf6
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-28 21:21:44 -0300

    Feat: implement method verificarSeLeilaoEstaCadastrado() in LeiloesPage

--- src/test/java/com/ndrewcoding/leilao/leiloes/LeiloesPage.java
+++ src/test/java/com/ndrewcoding/leilao/leiloes/LeiloesPage.java
@@ -1,6 +1,8 @@
 package com.ndrewcoding.leilao.leiloes;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class LeiloesPage {
 
@@ -18,6 +20,15 @@ public class LeiloesPage {
     public CadastroLeilaoPage carregarFormularioDeCadastroDeLeilao() {
         this.driver.navigate().to(URL_CADASTRO_LEILAO);
         return new CadastroLeilaoPage(driver);
+    }
+
+    public boolean verificarSeLeilaoEstaCadastrado(String nome, String dataAbertura, String valorInicial) {
+        WebElement ultimaLinhaDaTabelaDeLeiloes = this.driver.findElement(By.cssSelector("#tabela-leiloes tbody tr:last-child"));
+        WebElement colunaNome = ultimaLinhaDaTabelaDeLeiloes.findElement(By.cssSelector("td:nth-child(1)"));
+        WebElement colunaDataAbertura = ultimaLinhaDaTabelaDeLeiloes.findElement(By.cssSelector("td:nth-child(2)"));
+        WebElement colunaValorInicial = ultimaLinhaDaTabelaDeLeiloes.findElement(By.cssSelector("td:nth-child(3)"));
+
+        return colunaNome.getText().equals(nome) && colunaDataAbertura.getText().equals(dataAbertura) && colunaValorInicial.getText().equals(valorInicial);
     }
 
 }
