cad779d36a7d1e997697567e915637e6d4d97ec2
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-28 21:47:56 -0300

    Feat: implement basic structure of PageObject

--- src/test/java/com/ndrewcoding/leilao/PageObject.java
+++ src/test/java/com/ndrewcoding/leilao/PageObject.java
@@ -1,4 +1,20 @@
 package com.ndrewcoding.leilao;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class PageObject {
+
+    protected WebDriver webDriver;
+
+    public PageObject(WebDriver webDriver) {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+
+        this.webDriver = webDriver == null ? new ChromeDriver() : webDriver;
+    }
+
+    public void fechar() {
+        this.webDriver.quit();
+    }
+
 }
