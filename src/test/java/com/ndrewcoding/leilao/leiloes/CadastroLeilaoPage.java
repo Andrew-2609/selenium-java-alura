32b0364a84835769ed089cd8dd8275283b8bb051
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-28 21:22:10 -0300

    Feat: implement methods to handle fill and submit of formulary in CadastroLeilaoPage

--- src/test/java/com/ndrewcoding/leilao/leiloes/CadastroLeilaoPage.java
+++ src/test/java/com/ndrewcoding/leilao/leiloes/CadastroLeilaoPage.java
@@ -1,5 +1,6 @@
 package com.ndrewcoding.leilao.leiloes;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class CadastroLeilaoPage {
@@ -8,6 +9,17 @@ public class CadastroLeilaoPage {
 
     public CadastroLeilaoPage(WebDriver webDriver) {
         this.driver = webDriver;
+    }
+
+    public void preencherFormularioDeCadastro(String nome, String dataAbertura, String valorInicial) {
+        this.driver.findElement(By.id("nome")).sendKeys(nome);
+        this.driver.findElement(By.id("dataAbertura")).sendKeys(dataAbertura);
+        this.driver.findElement(By.id("valorInicial")).sendKeys(valorInicial);
+    }
+
+    public LeiloesPage submeterFormularioDeCadastro() {
+        this.driver.findElement(By.id("button-submit")).submit();
+        return new LeiloesPage(this.driver);
     }
 
 }
